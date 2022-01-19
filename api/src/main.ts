0e2758740f6787260a89fd226f8a8cbad65d4672
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-01-19 18:31:03 -0500

    logging test

--- api/src/main.ts
+++ api/src/main.ts
@@ -3,6 +3,12 @@ import { AppModule } from './app.module';
 const port = process.env.PORT || 3000;
 
 async function bootstrap() {
+  console.log(
+    `HOST=${process.env.DB_HOST}`,
+    `USERNAME=${process.env.DB_USERNAME}`,
+    `PASSWORD=${process.env.DB_PASSWORD}`,
+    `DATABASE=${process.env.DB_NAME}`,
+  )
   const app = await NestFactory.create(AppModule);
 
   await app.listen(port);
