e7f933637bd69a87ad58419fe22ce8a5badb762b
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-01-20 08:48:22 -0500

    cleaned up logging

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,35 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { config } from 'dotenv';
-import { NestApplicationOptions } from '@nestjs/common';
 
 const port = process.env.PORT || 3000;
-const appOptions: NestApplicationOptions = {};
-
-console.log('=============== BEFORE MAIN =============', process.env);
-if (process.env.NODE_ENV === 'production') {
-  console.log('==== ROOT USER ====');
-
-  config()
-  appOptions.logger = ['log', 'error', 'warn', 'debug','verbose'];
-} else {
-  console.log('===== NOT ROOT USER =====');
-  appOptions.logger = ['log', 'error', 'warn', 'debug','verbose'];
-  config({ path: '../.env' });
-}
-
-console.log(
-  '\n=====================================\n',
-  `HOST=${process.env.DB_HOST}\n`,
-  `USERNAME=${process.env.DB_USERNAME}\n`,
-  `PASSWORD=${process.env.DB_PASSWORD}\n`,
-  `DATABASE=${process.env.DB_NAME}\n`,
-  '\n=====================================\n'
-);
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, appOptions);
-  // console.log('-------------- ENV IN MAIN ---------------', process.env);
+  const app = await NestFactory.create(AppModule);
   await app.listen(port);
 }
 bootstrap();
