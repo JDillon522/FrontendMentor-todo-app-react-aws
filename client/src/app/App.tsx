002d245b54790d7d35240b13f28a196d93349026
Joseph Dillon <joseph.dillon.522@gmail.com>
2022-02-04 22:36:01 -0500

    remove drag and drop lib

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -3,9 +3,6 @@ import Header from '../header/Header';
 import { RecoilRoot } from 'recoil';
 import TodoCard from '../todo-card/TodoCard';
 import Yeet from '../yeet/Yeet';
-import { DndProvider } from 'react-dnd';
-import { TouchBackend } from 'react-dnd-touch-backend';
-import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
 
@@ -14,9 +11,7 @@ function App() {
       <Yeet />
       <div className='App'>
         <Header />
-        <DndProvider backend={HTML5Backend}>
           <TodoCard />
-        </DndProvider>
       </div>
     </RecoilRoot>
   );
